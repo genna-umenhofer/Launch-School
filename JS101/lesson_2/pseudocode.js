a7ed10ce88a94048d83a68b0e2bee9b6e1f85d77
Genna Umenhofer <gumenhofer@gmail.com>
2022-07-27 10:56:45 -0500

    added & configures ESLint

---
+++ JS101/lesson_2/pseudocode.js
@@ -0,0 +1,72 @@
+// a function that returns the sum of two numbers
+  // make a sum variable that is empty
+  // if the inputs are numbers, the sum is saved to the sum variable
+  // if they aren't numbers, run an error message
+  // return the sum variable (optional, print it out)
+/*
+START
+
+SET sum = empty
+GET number1 & number 2
+
+IF number1 & number 2
+  RETURN the sum of the two numbers
+
+ELSE
+  RETURN an error message
+
+END
+*/
+
+// a function that takes an array of strings and returns a string that is all those strings concantenated together
+  // make an empty string variable
+  // iterate through each element and condense it down to one string
+  // return the string variable (optional, print it out)
+/*
+START
+
+SET newString = empty
+GET array of strings from user
+
+push each element to the empty string
+
+RETURN the new string
+
+END
+*/
+
+// a method that takes an array of integers, and returns a new array with every other element from the original array, starting with the first element
+  // make an empty new array variable
+  // iterate through the input array starting at 0
+    // for even indexes, push them to the new array
+    // for odd indexes, continue without pushing them to the new array
+  // return the new array (optional, print it out)
+/*
+START
+
+END
+*/
+
+// a function that determines the index of the 3rd occurrence of a given character in a string. For instance, if the given character is 'x' and the string is 'axbxcdxex', the function should return 6 (the index of the 3rd 'x'). If the given character does not occur at least 3 times, return null.
+  // save the string as an array
+  // loop through the array to find a match for the character
+  // when a match is found, increment a counter up by one
+  // when the counter reaches 3, return the index of the location of the 3rd instance of the character
+  // if the index is more than the length of the string and the counter did not reach 3, then return null
+/*
+START
+
+END
+*/
+
+// a function that takes two arrays of numbers and returns the result of merging the arrays. The elements of the first array should become the elements at the even indexes of the returned array, while the elements of the second array should become the elements at the odd indexes.
+  // create a new empty array
+  // access the first array and push the first element to the new array at position 0
+   // access the second array and push the first element to the new array at position 1
+  // continue back and forth from one array to the other
+  // stop when the new array is the same length as both the arrays combined
+  /*
+START
+
+END
+*/
