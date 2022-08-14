b4fa7f0367204de658ee4e35d11343243651b8a1
Genna Umenhofer <gumenhofer@gmail.com>
2022-08-13 20:51:56 -0500

    two problems in easy 3

--- JS101/small_problems/easy_three.js
+++ JS101/small_problems/easy_three.js
@@ -1,14 +1,48 @@
 //ddaaiillyy ddoouubbllee ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function crunch(string) {
+  let words = string.split(' ');
+  return words.map(word => compareLetters(word)).join(' ');
+}
 
+function compareLetters(word) {
+  let reducedString = [];
+  let counter = 0;
+  while (counter < word.length) {
+    if (word[counter] !== word[counter + 1]) {
+      reducedString.push(word[counter]);
+      counter++;
+    } else {
+      counter++;
+    }
+  }
+  return reducedString.join('');
+}
+
+crunch('ddaaiilllyyy ddoouubbllee');
 
 //bannerizer ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+//skipping for now
 
 //stringy strings ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function stringy(num) {
+  let newString = [];
+  let toggle = 0;
+  for (let counter = num; counter > 0; counter -= 1) {
+    if (toggle === 0) {
+      newString.push('1');
+      toggle = 1;
+    } else {
+      newString.push('0');
+      toggle = 0;
+    }
+  }
+  return newString.join('');
+}
 
+console.log(stringy(6));    // "101010"
 
 //fibonacci number location by length ~~~~~~~~~~~~~~~~~~
-
+//skipping for now
 
 //right triangles ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
