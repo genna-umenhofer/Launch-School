9dab33a9a3c404407765a1ce7170287f4aabaf8b
Genna Umenhofer <gumenhofer@gmail.com>
2022-08-21 09:21:37 -0500

    finished doubles doubles

--- JS101/small_problems/easy_three.js
+++ JS101/small_problems/easy_three.js
@@ -115,7 +115,49 @@ console.log(`I learned a valuable lesson: Never leave home without your ${noun}.
 console.log(`If you ${verb} away wihtout it, your hair will turn ${adjective} ${adverb}.`);
 
 //doubles doubles ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+PROBLEM:
+- The input is an integer
+- The output is an integer
+  - If the number is a double number then the output is the double number
+  - If the output is not a double number then the output is the number multiplied by two
 
+- A double number is:
+  - an even number of digits (length 2, 4, 6, etc)
+  - the left-side digits (length/2) and right side digits are the same
+
+ALGORITHM:
+is number even digits? => are left & right halves equal? => return number
+
+any no answers return number x 2 instead
+
+num => evenDigits (helper function) => boolean
+- determine the remainder of the length of the number divided by 2
+- if it's 0 return true (it's even in length)
+- otherwise return false (it's not even in length)
+
+num => equalHalves (helper function) => boolean
+- save the left half of the number (length / 2) to a variable
+- save the right half of the number (length / 2) to a variable
+- return compare the two halves for strict equality
+*/
+
+function twice(num) {
+  if (evenDigits(num) && equalHalves(num)) return num;
+  return num * 2;
+}
+
+function evenDigits(num) {
+  if (num.toString().length % 2 === 0) return true;
+  return false;
+}
+
+function equalHalves(num) {
+  let halfNum = (num.toString().length) / 2
+  let leftHalf = num.toString().slice(0, halfNum);
+  let rightHalf = num.toString().slice(halfNum);
+  return leftHalf === rightHalf;
+}
 
 //grade book ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
