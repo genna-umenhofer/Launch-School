d79111eca7f70697ac15a8c771ef7c355a8f12b7
Genna Umenhofer <gumenhofer@gmail.com>
2022-08-20 20:43:06 -0500

    fixed and finished triangles

--- JS101/small_problems/easy_three.js
+++ JS101/small_problems/easy_three.js
@@ -47,112 +47,42 @@ console.log(stringy(6));    // "101010"
 //right triangles ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /*
 PROBLEM:
-- The input will be a positive integer 'n'
-- The output will be a string
-  - The string will be a right triangle made of stars '*'
-  - The hypotenuse will go from lower left to upper right
-  - All sides will have 'n' number of stars
+- The input is a positive integer
+- The output is a right triangle
 
-- length of string is always 'n'
-- first string has n-1 spaces before the star
-- second string has n-2 spaces... 
-- so on until n-n which is 'n' stars and no spaces
-
-EXAMPLES:
-triangle(5);
-
-    *
-   **
-  ***
- ****
-*****
+- Each line has one more '*' than the previous line
+- Each line has one less ' ' than the previous line
+- The number of lines is === the integer
+- ' ' go to the right of '*'
 
 DATA:
+n => '(n-1 spaces + 1 *'
+n => '(n-2 spaces + 2 *'
+...
+n => '(n-n spaces + n *'
 
-5 => 
-['    *'] =>
-['    *', '   **'] =>
-['    *', '   **', '  ***']=> 
-['    *', '   **', '  ***', ' ****'] =>
-['    *', '   **', '  ***', ' ****', '*****'] => 
-    *
-   **
-  ***
- ****
-*****
+n = integer passed into function.
+stars = integer subtracted from n to get spaces and represents the number of stars in a string => will be incremented by 1 for each time a line is run during the function
+
+empty string => loop that increments stars and adds to string => log the string => repeat loop until n === 0
 
 ALGORITHM:
-- initialize an empty array to store the star strings
-- if array length is < n
-  - addLineOfStars (helper function)
-    - determineStarNumber (helper function) [OUTPUT number of stars in next line]
-      - check the number of stars in the last array element
-        - initialize lastStarLine to array.length - 1
-        - make lastStarLin into an array of substrings
-        - iterate through lastStarLine 
-          - increment a counter if the element === *
-      - add one to that number
-      - output the new number
-    - makeStarString (helper function)
-      - initialize and empty string
-      - concatenate n-number spaces
-      - concatenate number of stars
-      - return the string of stars
-    -push the string of stars to the array
-- otherwise if array length === n
-  - for each element of the array, output the element
+START
+- 
 */
 
 function triangle(n) {
-  let triangleArray = [];
-
-  while (triangleArray.length < n) {
-    triangleArray.push(addLineOfStars(n, triangleArray));
-    console.log('triangArray = ' + triangleArray);
+  let stars = 1;
+  let spaces = ' ';
+  let aster = '*';
+  for (let counter = n; counter > 0; counter -= 1) {
+    let starString = spaces.repeat(n - stars) + aster.repeat(stars);
+    console.log(starString);
+    stars += 1;
   }
-  
-  triangleArray.forEach(elem => console.log(elem));
-  
-}
-
-function addLineOfStars(num, array) {
-  return makeStarString(determineStarNumber(array), num);
-}
-
-//let array = [];
-//console.log(addLineOfStars(4, array));
-
-function makeStarString(number, num) {
-  let starString = [];
-  for (let counter = (num - number); counter > 0; counter -= 1) {
-    starString.push(' ');
-  }
-  for (let counter = number; counter > 0; counter -= 1) {
-    starString.push('*');
-  }
-  console.log('starString = ' + starString.join(''));
-  return starString.join('');
-}
-
-function determineStarNumber(array) {
-  console.log('array length = ' + array.length);
-  if (array.length === 0) return 0;
-  if (array.length === 1) return 1;
-  /*
-  let lastStarLine = array[array.length - 1].split('');
-  let counter = 0;
-  lastStarLine.forEach(elem => {
-    if (elem === '*') {
-      counter += 1;
-    }
-  });
-  console.log(counter);
-  return counter;*/
-  return array[array.length - 1];
 }
 
 triangle(5);
-
 
 //madlibs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
