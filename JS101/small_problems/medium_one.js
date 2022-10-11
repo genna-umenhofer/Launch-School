c1dabcb66541f7da6b1f08cf0a3b17023d8b29ea
Genna Umenhofer <gumenhofer@gmail.com>
2022-10-10 19:59:02 -0500

    finished med problem

--- JS101/small_problems/medium_one.js
+++ JS101/small_problems/medium_one.js
@@ -90,7 +90,100 @@ console.log(rotateRightmostDigits(735291, 6));
 
 
 //word to digit ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+Problem:
+Input: string
+Output: string
 
+- The output should have all number words replace with digits
+- 'five' becomes 5
+- assuming 0-9 digits
+- all words are separated by spaces
+- need to remove punctuation somehow? Regex?
+
+Data:
+string => array => iterate => switch out number words => join => string
+
+Algorithm:
+- save a new empty array
+- save the string as an array of words
+- iterate through the array
+
+- check for punctuation is true
+  - save the word as a variable
+  - save the punctuation as a variable
+  - push the word as a number
+  - push the punctuation
+
+- if check punctuation is false
+  - if the word matches, push the number instead
+  - otherwise, push the current word
+  
+- return new array joined together
+
+word => check punctuation => punctuation
+- see if the last index is a letter
+- if it is, return false
+- if not, return true (has punctuation)
+
+word => swap for number => number string
+- switch statement, check word for each number
+- return number string or just word (default)
+*/
+
+function wordToDigit(string) {
+  let newArray = [];
+  let stringArray = string.split(' ');
+
+  stringArray.forEach(word => {
+    if (checkPunctuation(word)) {
+      let separateWord = word.slice(0, (word.length - 1));
+      let punctuationMark = word[word.length-1];
+
+      newArray.push(swapForNumber(separateWord) + punctuationMark);
+      
+    } else {
+      newArray.push(swapForNumber(word));
+    }
+  });
+
+  return newArray.join(' ');
+}
+
+function checkPunctuation(word) {
+  let idx = (word.length) - 1;
+  let separateWord = word[idx];
+  return (!(separateWord >= 'a' && separateWord <= 'z'));
+}
+
+function swapForNumber(word) {
+  switch(word) {
+    case 'one':
+      return '1';
+    case 'two':
+      return '2';
+    case 'three':
+      return '3';
+    case 'four':
+      return '4';
+    case 'five':
+      return '5';
+    case 'six':
+      return '6';
+    case 'seven':
+      return '7';
+    case 'eight':
+      return '8';
+    case 'nine':
+      return '9';
+    case 'zero':
+      return '0';
+    default:
+      return word;
+  }
+}
+
+console.log(wordToDigit('The weight is done.'));
 
 //fib nums recursion ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
